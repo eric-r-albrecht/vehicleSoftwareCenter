6119289946c93fec3dba498a90fc9cdd18feba0c
eric-r-albrecht <enrique3399@gmail.com>
2019-08-11 22:45:15 -0500

    Set initial Vehicle characteristics, and created Car.

--- GryphonCode/src/main/java/vehicles/Car.java
+++ GryphonCode/src/main/java/vehicles/Car.java
@@ -1,4 +1,53 @@
 package vehicles;
 
-public class Car {
+import vehiclePropulsion.PropulsionDevice;
+import vehiclePropulsion.PropulsionSystem;
+
+import static Utilities.Verification.*;
+
+/**
+ * Car definition for Vehicles
+ */
+public class Car extends Vehicle {
+
+    /**
+     * Car Error Messages
+     */
+    private static final String CAR = "Car";
+    private static final String CAR_LICENSE_PLATE = "Car License Plate";
+
+
+    /**
+     * Vehicle Constructor Parameters
+     */
+    private static final String CAR_PROPULSION_DEVICE_NAME = "Typical Car Base";
+    private static final int CAR_PROPULSION_DEVICE_QUANTITY = 4;
+
+    /**
+     * Car Class Variables
+     */
+    private String make;
+    private String model;
+    private String licensePlate;
+
+    /**
+     * Car Constructor using vehicle inherited constructor.
+     * @param make make of the car
+     * @param model model of the car
+     * @param licensePlate license plate of the car
+     */
+    public Car(String make, String model, String vehicleIdentification, String licensePlate){
+        super(new PropulsionSystem(new PropulsionDevice(PropulsionDevice.DeviceType.Wheels, CAR_PROPULSION_DEVICE_QUANTITY, CAR_PROPULSION_DEVICE_NAME)), model, make, vehicleIdentification, CAR);
+        notNull(licensePlate, CAR_LICENSE_PLATE);
+        this.licensePlate = licensePlate;
+    }
+
+    /**
+     * get license plate of the car.
+     * @return license plate.
+     */
+    public String getLicensePlate(){
+        return licensePlate;
+    }
+
 }
--- GryphonCode/src/main/java/vehicles/Vehicle.java
+++ GryphonCode/src/main/java/vehicles/Vehicle.java
@@ -2,16 +2,86 @@ package vehicles;
 
 import vehiclePropulsion.PropulsionSystem;
 
+import static Utilities.Verification.*;
+
 /**
  * Base Class for vehicles constructed.
  */
-abstract class Vehicle {
+public abstract class Vehicle {
 
     /**
-     * Propulsion System used for Vehicle.
+     * Vehicle Error Messages
+     */
+    private static final String VEHICLE_PROPULSION_SYSTEM = "Propulsion System";
+    private static final String VEHICLE_MODEL = "Model";
+    private static final String VEHICLE_MAKE = " Make";
+    private static final String VEHICLE_VIN = " Identification Number";
+    private static final String VEHICLE_TYPE_NAME = "Vehicle Type Name";
+
+    /**
+     * Vehicle Class Variables
      */
     private PropulsionSystem propulsionSystem;
+    private String model;
+    private String make;
+    private String vehicleIdentification;
+    private String vehicleTypeName;
+
     /**
-     * Other major vehicle characteristics will go here in the long run of development.
+     * Default constructor for a vehicle, defining propulsion system
+     * @param propulsionSystem propulsion system for vehicle
      */
+    public Vehicle(PropulsionSystem propulsionSystem, String model, String make, String vehicleIdentification, String vehicleTypeName) {
+        notNull(vehicleTypeName, VEHICLE_TYPE_NAME);
+        notNull(propulsionSystem, vehicleTypeName + VEHICLE_PROPULSION_SYSTEM);
+        notNull(model, vehicleTypeName + VEHICLE_MODEL);
+        notNull(make, vehicleTypeName + VEHICLE_MAKE);
+        notNull(vehicleIdentification, vehicleTypeName + VEHICLE_VIN);
+        this.vehicleTypeName = vehicleTypeName;
+        this.propulsionSystem = propulsionSystem;
+        this.model = model;
+        this.vehicleIdentification = vehicleIdentification;
+        this.make = make;
+    }
+
+    /**
+     * Get the vehicle model.
+     * @return vehicle model.
+     */
+    public String getVehicleModel(){
+        return model;
+    }
+
+    /**
+     * Get the vehicle make.
+     * @return vehicle make.
+     */
+    public String getVehicleMake(){
+        return make;
+    }
+
+    /**
+     * Get the vehicle identification number.
+     * @return vehicle identification number.
+     */
+    public String getVehicleVin(){
+        return vehicleIdentification;
+    }
+
+    /**
+     * Get the vehicle type name.
+     * @return vehicle type name.
+     */
+    public String getVehicleTypeName(){
+        return vehicleTypeName;
+    }
+
+    /**
+     * Get the vehicle propulsion system.
+     * @return vehicle propulsion system.
+     */
+    public PropulsionSystem getPropulsionSystem(){
+        return propulsionSystem;
+    }
+
 }
