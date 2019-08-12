4a177e85a163e15d41878f4aa5d6e5245f35e16b
eric-r-albrecht <enrique3399@gmail.com>
2019-08-12 00:46:39 -0500

    updated test driver to test the car functionality.

--- GryphonCode/src/main/java/Main.java
+++ GryphonCode/src/main/java/Main.java
@@ -1,6 +1,13 @@
+import vehicles.Car;
+import vehicles.Vehicle;
+
+import static vehicles.VehicleFactory.createVehicle;
+
 public class Main {
 
     public static void main(String[] args){
-        System.out.println("Hello world!");
+        String[] carparams = {"Pontiac", "G5", "123456", "XPV499"};
+        Vehicle vehicle = createVehicle(Car.class, carparams);
+        System.out.println(vehicle.printFullVehicle());
     }
 }
